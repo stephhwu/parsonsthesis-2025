5e48dc400107ea58268397ec4fa95a4f05820699
stephhwu <wus611@newschool.edu>
2025-05-08 19:01:15 -0400

    added missing /

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/parsonsthesis-2025',
+  base: '/parsonsthesis-2025/',
   plugins: [vue()],
   resolve: {
     alias: {
